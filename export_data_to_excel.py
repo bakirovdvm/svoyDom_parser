8082401fa980f3d4ab0698c4a9455c76110a8a35
Sergali Bakirov <bakirovdvm@gmail.com>
2024-08-02 00:44:28 +0600

    i can update book, but need to try open, if no book then create new - need to work ir

--- export_data_to_excel.py
+++ export_data_to_excel.py
@@ -1,62 +1,62 @@
-import openpyxl
-import json
-import datetime
-
-# print(datetime.date.today())
-
-
-with open('result_zhana_urpaq.json') as file:
-    data = json.load(file)
+# import openpyxl
+# import json
+# import datetime
 #
+# # print(datetime.date.today())
+#
+#
+# with open('result_zhana_urpaq.json') as file:
+#     data = json.load(file)
+# #
+# # for item in data:
+# #     apt_number = item['Номер квартиры:']
+# #     rooms_quality = item['Количество комнат:']
+# #     apt_S = item['Площадь:']
+# #     price_sq_m = item['Цена за квм:']
+# #     price_total = item['Цена общая:']
+# #     floor = item['Этаж:']
+# #     entrance = item['Подъезд:']
+# #     print(apt_number, rooms_quality, apt_S, price_sq_m, price_total, floor, entrance)
+#
+# book = openpyxl.Workbook()
+# sheet = book.active
+#
+# sheet['A3'] = 'APT_NUMBER'
+# sheet['B3'] = 'ROOMS_Q'
+# sheet['C3'] = 'APT_S'
+# sheet['D3'] = 'FLOOR'
+# sheet['E3'] = 'ENTRANCE'
+# sheet['G3'] = 'DATE'
+# sheet['H3'] = 'PRICE_SQ_M'
+# sheet['I3'] = 'PRICE_TOTAL'
+#
+# row = 4
 # for item in data:
-#     apt_number = item['Номер квартиры:']
-#     rooms_quality = item['Количество комнат:']
-#     apt_S = item['Площадь:']
-#     price_sq_m = item['Цена за квм:']
-#     price_total = item['Цена общая:']
-#     floor = item['Этаж:']
-#     entrance = item['Подъезд:']
-#     print(apt_number, rooms_quality, apt_S, price_sq_m, price_total, floor, entrance)
-
-book = openpyxl.Workbook()
-sheet = book.active
-
-sheet['A3'] = 'APT_NUMBER'
-sheet['B3'] = 'ROOMS_Q'
-sheet['C3'] = 'APT_S'
-sheet['D3'] = 'FLOOR'
-sheet['E3'] = 'ENTRANCE'
-sheet['G3'] = 'DATE'
-sheet['H3'] = 'PRICE_SQ_M'
-sheet['I3'] = 'PRICE_TOTAL'
-
-row = 4
-for item in data:
-    sheet[row][0].value = item['Номер квартиры:']
-    sheet[row][1].value = item['Количество комнат:']
-    sheet[row][2].value = item['Площадь:']
-    sheet[row][3].value = item['Этаж:']
-    sheet[row][4].value = item['Подъезд:']
-    sheet[row][6].value = datetime.date.today()
-    if sheet[row][7]:
-        sheet[row][7].value = item['Цена за квм:']
-        # sheet[row][8].value = item['Цена общая:']
-        sheet[row][8].value = int(item['Цена за квм:'].replace(' ', '')) * float(item['Площадь:'])
-    # print(apt_number, rooms_quality, apt_S, price_sq_m, price_total, floor, entrance)
-    else:
-        sheet[row][7].value = item['Цена за квм:']
-        sheet[row][8].value = item['Цена общая:']
-    row += 1
-
-
-book.save('my_book.xlsx')
-book.close()
-
-print('done')
+#     sheet[row][0].value = item['Номер квартиры:']
+#     sheet[row][1].value = item['Количество комнат:']
+#     sheet[row][2].value = item['Площадь:']
+#     sheet[row][3].value = item['Этаж:']
+#     sheet[row][4].value = item['Подъезд:']
+#     sheet[row][6].value = datetime.date.today()
+#     if sheet[row][7]:
+#         sheet[row][7].value = item['Цена за квм:']
+#         # sheet[row][8].value = item['Цена общая:']
+#         sheet[row][8].value = int(item['Цена за квм:'].replace(' ', '')) * float(item['Площадь:'])
+#     # print(apt_number, rooms_quality, apt_S, price_sq_m, price_total, floor, entrance)
+#     else:
+#         sheet[row][7].value = item['Цена за квм:']
+#         sheet[row][8].value = item['Цена общая:']
+#     row += 1
+#
+#
+# book.save('my_book.xlsx')
+# book.close()
+#
+# print('done')
 
 
 
-####### NEED TO STUDY CODE #######
+####### NEED TO STUDY CODE ############## NEED TO STUDY CODE ############## NEED TO STUDY CODE ############## NEED TO STUDY CODE
 # https://stackoverflow.com/questions/67880607/how-to-update-existing-excel-xlsx-file-using-openpyxl
 # import arcpy
 # import openpyxl as px
@@ -84,3 +84,69 @@ print('done')
 #
 # if __name__ == "__main__":
 #     main()
+####### NEED TO STUDY CODE ############## NEED TO STUDY CODE ############## NEED TO STUDY CODE ############## NEED TO STUDY CODE
+
+
+
+
+import openpyxl
+import json
+import datetime
+
+# print(datetime.date.today())
+def main():
+
+    with open('result_zhana_urpaq.json') as file:
+        data = json.load(file)
+    #
+    # for item in data:
+    #     apt_number = item['Номер квартиры:']
+    #     rooms_quality = item['Количество комнат:']
+    #     apt_S = item['Площадь:']
+    #     price_sq_m = item['Цена за квм:']
+    #     price_total = item['Цена общая:']
+    #     floor = item['Этаж:']
+    #     entrance = item['Подъезд:']
+    #     print(apt_number, rooms_quality, apt_S, price_sq_m, price_total, floor, entrance)
+
+
+    book = openpyxl.load_workbook(r'my_new_book.xlsx')
+
+    sheet = book.active
+
+    sheet['A3'] = 'APT_NUMBER'
+    sheet['B3'] = 'ROOMS_Q'
+    sheet['C3'] = 'APT_S'
+    sheet['D3'] = 'FLOOR'
+    sheet['E3'] = 'ENTRANCE'
+    sheet['G3'] = 'DATE'
+    sheet['H3'] = 'PRICE_SQ_M'
+    sheet['I3'] = 'PRICE_TOTAL'
+
+    # row = 4
+    # for item in data:
+    #     sheet[row][0].value = item['Номер квартиры:']
+    #     sheet[row][1].value = item['Количество комнат:']
+    #     sheet[row][2].value = item['Площадь:']
+    #     sheet[row][3].value = item['Этаж:']
+    #     sheet[row][4].value = item['Подъезд:']
+    #     sheet[row][6].value = datetime.date.today()
+    #     if sheet[row][7]:
+    #         sheet[row][7].value = item['Цена за квм:']
+    #         # sheet[row][8].value = item['Цена общая:']
+    #         sheet[row][8].value = int(item['Цена за квм:'].replace(' ', '')) * float(item['Площадь:'])
+    #     # print(apt_number, rooms_quality, apt_S, price_sq_m, price_total, floor, entrance)
+    #     else:
+    #         sheet[row][7].value = item['Цена за квм:']
+    #         sheet[row][8].value = item['Цена общая:']
+    #     row += 1
+
+    book.save(r'my_new_book.xlsx')
+    book.close()
+    # book.save('my_book.xlsx')
+    # book.close()
+    #
+    print('done')
+
+if __name__ == "__main__":
+    main()
